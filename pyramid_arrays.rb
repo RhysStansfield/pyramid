ac50efd098f0fddbd7e50d56d93d8432f967fb60
Rhys Stansfield <rhys@onlinesalesgrowth.com>
2013-10-09 12:38:42 +0100

    First commit - pyramid array file and README

---
+++ pyramid_arrays.rb
@@ -0,0 +1,25 @@
+def pyramid(number)
+  array_of_arrays = []
+  while number>0
+    array_of_arrays << array(number)
+    number -= 1
+  end
+  array_of_arrays.reverse
+end
+
+def array(number)
+  array = (1..number).map { |number| number = 1 }
+end
+
+
+describe 'Pyramid of Arrays' do
+  it 'Can create an array with three items if given the number three' do
+    expect(pyramid(3)).to eq [1,1,1]
+  end
+  it 'Can create an array with two items if given the number two' do
+    expect(pyramid(2)).to eq [1, 1]
+  end
+  it 'Can create an array with one item if given the number one' do
+    expect(pyramid(1)).to eq [1]
+  end
+end
